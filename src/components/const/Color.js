893b0b959d340ae022dce364e99cd2ca7afc0f98
小倉優悟 <jigenbakdaaan@gmail.com>
2018-10-18 01:12:11 +0900

    update タグのスタイル変更

---
+++ src/components/const/Color.js
@@ -0,0 +1,10 @@
+// const/Color.js
+export default {
+  Lightx: '#b2d8d8'
+  Light: '#66b2b2'
+  Normal: '#008080'
+  Heavy: '#006666'
+  Heavyx: '#004c4c'
+  Gray: '#4a4a4a'
+  Primary: '#008080'
+}
